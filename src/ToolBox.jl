fac6a868998d6d7b31fe14744b168aebac724247
Peter Fitton - TheBreadFarmer <phfitton@gmail.com>
2024-06-20 14:33:19 -0400

    Loaded all current code into the package.

--- src/ToolBox.jl
+++ src/ToolBox.jl
@@ -1,5 +1,163 @@
 module ToolBox
 
-# Write your package code here.
+using LorentzVectors, LorentzVectorHEP, GLMakie, FHist, Distributions
+
+
+include("./statutils.jl")
+include("./plottingutils.jl")
+
+
+export W_approx, W_exact
+export efficiency, purity, max_purity, cut_stats
+export smear, get_acceptance
+export make_stackedhist
+
+
+
+
+
+"""
+    W_approx(l_in::LorentzVector, l_out::LorentzVector; Mn::Float64=938.91875433)
+
+Compute the approximate hadronic invariant mass W.
+
+```math
+W = √{ -(-q)² + 2⋅Mn⋅ω + Mn² }
+ q = l_in - l_out
+ ω = El_in - El_out
+```
+
+W is calculated from the incoming lepton `l_in` and the outgoing final state primary lepton `l_out`. 
+It is also assumed that the struck nucleon is at rest when struck. 
+`Mn` is the mass (in MeV) of the struck nucleon as the average between the masses of the neutron and proton reported by PDG.
+The units of the returned value for W will be the same as the units of the argument value.
+
+See also [`W_exact`](@ref)
+"""
+function W_approx(l_in::LorentzVector, l_out::LorentzVector; Mn::Float64=938.91875433)
+    # calculate W using approximation. if the argument of sqrt() is negative, return 0.0 instead.
+    # `mass(l_in - l_out)^2` is equivalent to Q^2.
+    W = try sqrt(-mass(l_in - l_out)^2 + 2*(Mn/1000)*(energy(l_in) - energy(l_out)) + (Mn/1000)^2) catch; 0.0 end # units of GeV
+
+
+    return W
+end
+
+"""
+    W_exact(l_in::LorentzVector, l_out::LorentzVector, n_in::LorentzVector)
+
+Compute the hadronic invariant mass W.
+
+```math
+W = √{ -(-q)² + 2( q ⋅ n_in ) + Mn² }
+q = l_in - l_out
+```
+
+W is calculated from the four-momentum of the incoming lepton `l_in`,
+the outgoing final state primary lepton `l_out`,
+and the struck/incoming nucleon `n_in`.
+This function does not assume that the struck nucleon is at rest.
+The units of the returned value for W will be the same as the units of the argument value.
+
+See also [`W_approx`](@ref)
+"""
+function W_exact(l_in::LorentzVector, l_out::LorentzVector, n_in::LorentzVector)
+    # calculate W using exact values. `mass(l_in - l_out)^2` is equivalent to Q^2.
+    W = sqrt(-mass(l_in - l_out)^2 + 2*dot((l_in - l_out), n_in) + mass(n_in)^2) # units of GeV
+
+
+    return W
+end
+
+
+
+"""
+    research_dir()
+
+Return absolute path to primary directory of research project. Throw error if no such path exists.
+
+CAUTION: Make sure this path is kept up to date if the directory is moved, altered, or folder names are changed.
+
+See also [`plot_tempdir`](@ref)
+"""
+function research_dir()
+    path = raw"C:/Users/phfit/Desktop/Summer 2024/Research/"
+    isdir(path) ? string(path) : error("$(path) could not be found.\nPlease update variable `path` in function $(string(nameof(research_dir)))()\n    @ \"$(functionloc(research_dir)[1])\":$(functionloc(research_dir)[2])")
+end
+
+
+"""
+    plot_tempdir()
+
+Return absolute path to "~/plotlib/temp" directory. Throw error if no such path exists.
+
+CAUTION: Make sure this path is kept up to date if the directory is moved, altered, or folder names are changed.
+
+See also [`research_dir`](@ref)
+"""
+function plot_tempdir()
+    path = string(research_dir(), raw"/plotlib/temp/")
+    isdir(path) ? string(path) : error("$(path) could not be found.\nPlease update variable `path` in function $(string(nameof(plot_tempdir)))()\n    @ \"$(functionloc(plot_tempdir)[1])\":$(functionloc(plot_tempdir)[2])")
+end
+
+
+
+### Particle Smearing
+"""
+    smear(particle::LorentzVector, resolution::Float64)
+
+Return smeared reconstructed particle 4-momentum. Resolution is the % resolution in decimal form.
+"""
+function smear(particle::LorentzVector, resolution::Float64=0.0005)
+    # compute particles true total momentum.
+    p_true = LorentzVectorHEP.mag(particle) # units of GeV
+
+    # compute standard deviation from resolution and true total momentum.
+    σ = p_true * resolution # units of GeV
+
+    # randomly smear the total momentum.
+    p_reco = rand(Normal(p_true, σ)) # units of GeV
+
+    # compute smear ratio.
+    r = p_reco / p_true
+
+    # reconstruct smeared particle energy.
+    m2 = LorentzVectorHEP.mass2(particle)
+    E_reco = sqrt(m2 + r^2*p_true^2)
+
+    # reconstruct smeared particle 3-momentum components.
+    x_reco, y_reco, z_reco = r*particle.x, r*particle.y, r*particle.z
+
+    # pack reconstructed components into 4-momentum.
+    particle_reco = LorentzVector(E_reco, x_reco, y_reco, z_reco)
+
+    # return reconstructed particle
+    return particle_reco
+end
+
+"""
+    smear(p_true::Float64, resolution::Float64)
+
+Return smeared reconstructed total momentum magnitude. Resolution is the % resolution in decimal form.
+"""
+function smear(p_true::Float64, resolution::Float64)
+    # compute standard deviation from resolution and true total momentum.
+    σ = p_true * resolution # units of GeV
+
+    # randomly smear the total momentum.
+    p_reco = rand(Normal(p_true, σ)) # units of GeV
+
+    # return the smeared reconstruction of total momentum.
+    return p_reco
+end
+
+
+
+### particle acceptance
+function get_acceptance(lv::LorentzVector)
+    cos_theta = LorentzVectorHEP.CosTheta(lv)
+
+    return ifelse(0.35 < cos_theta && cos_theta < 0.98, 1.0, 0.0)
+end
 
 end
---
+++ src/statutils.jl
@@ -0,0 +1,177 @@
+# TODO: add `nothing` bound functionality.
+# TODO: consider renaming to something more clear.
+# cuts from the lower edge of x1 and the upper edge of x2. i.e. cut does not include the bin starting at x2 or the bin ending at x1.
+"Compute the efficiency of selecting signal events from a larger sample for the given cut."
+function efficiency(sample::Vector{Hist1D{T}}, signal_index::Int64,
+                    x1::Number, x2::Number; print_output::Bool=true) where {T}
+
+    # get hist of selected signal events, where bounds are restricted by cut.
+    restricted_signal_hist = restrict(sample[signal_index], x1, x2)
+
+    # calculate the number of signal events selected by the cut.
+    nevents_signal_cut = integral(restricted_signal_hist)
+
+    # calculate the total number of signal events.
+    nevents_signal_total = nentries(sample[signal_index])
+
+    # compute efficiency
+    _efficiency = nevents_signal_cut / nevents_signal_total
+
+    if print_output == true
+        println("efficiency = $(round(_efficiency; digits=4)*100)%")
+    end
+
+    # calculate and return the efficiency of these bounds.
+    return _efficiency
+end
+
+# TODO: add `nothing` bound functionality.
+# TODO: consider renaming to something more clear.
+# cuts from the lower edge of x1 and the upper edge of x2. i.e. cut does not include the bin starting at x2 or the bin ending at x1.
+"Compute the purity of selected events from given sample events with respect to given signal events."
+function purity(sample::Vector{Hist1D{T}}, signal_index::Int64, x1::Number, x2::Number;
+                print_output::Bool=true) where {T}
+
+    # get hist of selected signal events, where bounds are restricted by cut.
+    restricted_signal_hist = restrict(sample[signal_index], x1, x2)
+
+    # calculate the number of signal events selected by the cut.
+    nevents_signal_cut = integral(restricted_signal_hist)
+
+    # calculate the total number of events selected by the cut.
+    # HOW?
+    # 1.create a vector of the number of entries for each restricted hist from `sample`, restricted to select values between cut.
+    # 2.sum all elements of that vector to compute the total number of events selected by the cut.
+    nevents_all_cut = sum([integral(restrict(sample[i], x1, x2)) for i in eachindex(sample)])
+
+    # compute the purity for these bounds.
+    purity = nevents_signal_cut / nevents_all_cut
+
+    if print_output == true
+        println("Purity = $(round(purity; digits=4)*100)%")
+    end
+
+    # if purity is NaN replace with zero and return, else return purity.
+    isnan(purity) && return 0.0
+    return purity
+end
+
+"Compute maximum possible purity of the given sample with respect to the given signal."
+function max_purity(sample::Vector{Hist1D{T}}, signal_index::Int64;
+                    return_edges::Bool=false, print_output::Bool=true) where {T}
+
+    highest_purity_value = 0.0
+
+    highest_purity_bin_edges = (0.0, 0.0)
+
+    _edges = binedges(sample[signal_index])[begin:end]
+
+    # creates pairs up to end of range. is itterated in reference to the lower edge.
+    each_bin_edges = tuple.(_edges[1:end-1], _edges[2:end])
+
+    for bin in each_bin_edges
+        bin_purity = purity(sample, signal_index, bin[1], bin[2]; print_output=false)
+
+        bin_purity > highest_purity_value && 
+        (highest_purity_value=bin_purity; highest_purity_bin_edges=bin)
+    end
+
+    if print_output == true
+        println("Max Purity = $(round(highest_purity_value; digits=4)*100)%")
+
+        if return_edges == false
+            return highest_purity_value
+        else
+            println("    @ Bounds = $(highest_purity_bin_edges)")
+
+            return highest_purity_value, highest_purity_bin_edges
+        end
+        
+    end
+
+    if return_edges == false
+        return highest_purity_value
+    else
+        return highest_purity_value, highest_purity_bin_edges
+    end
+end
+
+# Optionally supply a lower and upper bound as fourth and fith argument to preview stats within that bound.
+# If no bounds are passed, it will compute the best bounds internaly.
+"Returns useful stats regarding cuting a sample by W with respect to a given signal."
+function cut_stats(sample::Vector{Hist1D{T}}, signal_index::Int64,
+                    target_purity::Float64; print_output::Bool=true) where {T}
+
+    _max = PhyLib.max_purity(sample, signal_index; return_edges=false, print_output=false)
+
+    target_purity > _max && error("Keyword argument \"target_purity\" must 
+        be below the max purity for the given sample and signal.")
+
+    purity, bounds = best_cut_purity(sample, signal_index, target_purity; print_output=false)
+
+    eff = efficiency(sample, signal_index, bounds...; print_output=false)
+
+    width = bounds[2] - bounds[1]
+    
+    if print_output == true
+        println("Purity = $(round(purity*100; digits=4))%")
+        println("Efficiency = $(round(eff*100; digits=4))%")
+        println("Bounds = $(bounds)")
+        println("Width = $(width)")
+    end
+
+    return (purity = purity, efficiency = eff, bounds = bounds, width = width)
+end
+
+# This function does not account for overflow bins.
+function best_cut_purity(sample::Vector{Hist1D{T}}, signal_index::Int64,
+                        target_purity::Float64; print_output::Bool=true) where {T}
+
+    # define variable for chosen purity.
+    chosen_purity = 0
+
+    # define variable for best bounds.
+    best_bounds = (0,0)
+
+    # get bin stepsize and upper bin edge.
+    step_size = step(binedges(sample[signal_index]).uniform_edges)
+    upper_edge = last(binedges(sample[signal_index]))-step_size
+
+    # get range for possible lower bound valeus from first bin to last bin.
+    lower_bounds = (first(binedges(sample[signal_index])) + step_size):(step_size):(upper_edge)
+
+    # step through each lower bound.
+    for lower_bound in lower_bounds # for each lower bound...
+        # get range for possible upper bound values from lower_bound to upper_edge.
+        upper_bounds = (lower_bound + step_size):(step_size):(upper_edge)
+
+        # step through each upper bound.
+        for upper_bound in upper_bounds # for each lower and upper bound pair...
+            # compute purity for given upper_bound and lower_bound.
+            bound_purity = purity(sample, signal_index, lower_bound, upper_bound; print_output=false)
+
+            # check purity is greater than minimum set by target_purity.
+            if bound_purity >= target_purity # purity greater than or equal to target_purity.
+                # check width between lower_bound and upper_bound is greater than width between "best" bounds.
+                if upper_bound - lower_bound > best_bounds[2] - best_bounds[1]
+                    # set best_bounds to lower and upper bounds.
+                    best_bounds = (lower_bound, upper_bound)
+
+                    # set chosen_purity to purity.
+                    chosen_purity = bound_purity
+                end
+            end
+        end
+    end
+
+    # compute width of bounds
+    width = best_bounds[2] - best_bounds[1]
+
+    if print_output == true
+        println("Purity = $(round(chosen_purity; digits=4))")
+        println("Bounds = $(best_bounds)")
+        println("Width = $(width)")
+    end
+
+    return chosen_purity, best_bounds
+end

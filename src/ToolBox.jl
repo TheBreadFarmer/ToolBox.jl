e8f03971e394df44d489591f1b26f58997b33fd5
Peter Fitton - TheBreadFarmer <phfitton@gmail.com>
2024-10-11 10:04:40 -0400

    Update ToolBox.jl

--- src/ToolBox.jl
+++ src/ToolBox.jl
@@ -7,6 +7,8 @@ export W_approx, W_exact
 export efficiency, purity, maxpurity, cut_stats
 export smear, get_acceptance
 
+export AVG_NUCLEON_MASS, BINDING_ENERGY, NEUTRON_MASS, PROTON_MASS
+
 
 include("physicalconstants.jl")
 include("statisticsutils.jl")

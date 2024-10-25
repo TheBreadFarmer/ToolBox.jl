ec90708d5a30d5001b2d8b5f1c5cdea7fa492a4e
Peter Fitton - TheBreadFarmer <phfitton@gmail.com>
2024-10-25 12:50:10 -0400

    Add Docstrings to constants

--- src/constants/physicalconstants.jl
+++ src/constants/physicalconstants.jl
@@ -1,8 +1,12 @@
 
+# FIXME check if BINDING_ENERGY is specifically for 12-Carbon.
+"Average binding energy for nucleon in (12-Carbon?) nucleus. Units [GeV]"
 BINDING_ENERGY = -0.0315 # GeV
 
+"Units [GeV]"
 NEUTRON_MASS = 0.9395654205 # GeV
+"Units [GeV]"
 PROTON_MASS = 0.93827208816 # GeV
 
-# truncated to 10 significant digits to keep accuracy of masses used to compute value.
+"truncated to 10 significant digits to keep accuracy of masses used to compute value. Units [GeV]"
 AVG_NUCLEON_MASS = trunc((PROTON_MASS+NEUTRON_MASS)/2,sigdigits=10) # GeV
